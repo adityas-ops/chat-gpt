9bc7afcee2bfa997db97f46a518d4b378f532857
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-02-04 01:12:50 +0530

    add

---
+++ client/src/App.jsx
@@ -0,0 +1,182 @@
+import { useState, useEffect } from "react";
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import loadingIcon from "./assets/loader.svg";
+import bot from "./assets/bot.png";
+import axios from "axios";
+import { FileUploader } from "react-drag-drop-files";
+import Tesseract from "tesseract.js";
+
+function App() {
+  const fileTypes = ["JPG", "PNG", "GIF"];
+  const [file, setFile] = useState(null);
+  const [input, setInput] = useState("");
+  const [posts, setPost] = useState([]);
+
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  useEffect(() => {
+    document.querySelector(".layout").scrollTop =
+      document.querySelector(".layout").scrollHeight;
+  }, [posts]);
+
+  const fetchBotResponse = async () => {
+    const { data } = await axios.post(
+      "http://localhost:4001",
+      { input },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return data;
+  };
+
+  const onSubmits = () => {
+    if (input.trim() === "") {
+      return;
+    }
+    updatePost(input);
+    updatePost("Loading", false, true);
+    fetchBotResponse().then((res) => {
+      updatePost(res.bot.trim(), true);
+      console.log(res);
+    });
+    setInput("");
+  };
+
+  const autoTypingBotResponse = (text) => {
+    let index = 0;
+    let interval = setInterval(() => {
+      if (index < text.length) {
+        setPost((prevState) => {
+          let lastItem = prevState.pop();
+          if (lastItem.type !== "bot") {
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index - 1),
+            });
+          } else {
+            prevState.push({
+              type: "bot",
+              post: lastItem.post + text.charAt(index - 1),
+            });
+          }
+          return [...prevState];
+        });
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 30);
+  };
+
+  const updatePost = (post, isBot, isLoading) => {
+    if (isBot) {
+      console.log(post);
+      autoTypingBotResponse(post);
+    } else {
+      setPost((PrevState) => {
+        return [
+          ...PrevState,
+          { type: isLoading ? "loading" : "user", post: post },
+        ];
+      });
+    }
+  };
+
+  const onKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      onSubmits();
+    }
+  };
+
+  return (
+    <>
+      <main className="chatGPT-app">
+        <section className="chat-container">
+          <div className="layout">
+            {posts.map((post, index) => {
+              return (
+                <>
+                  <div
+                    key={index}
+                    className={`chat-bubble ${
+                      post.type === "bot" || post.type === "loading"
+                        ? "bot"
+                        : " "
+                    }`}
+                  >
+                    <div className="avatar">
+                      <img
+                        src={
+                          post.type === "bot" || post.type === "loading"
+                            ? bot
+                            : user
+                        }
+                        alt="user"
+                      />
+                    </div>
+                    {post.type === "loading" ? (
+                      <div className="loader">
+                        <img src={loadingIcon} alt="loading" />
+                      </div>
+                    ) : (
+                      <div className="post">{post.post}</div>
+                    )}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </section>
+        <footer>
+          <div className="footer1">
+            <div className="file">
+              <div>
+                <FileUploader
+                  handleChange={handleChange}
+                  name="file"
+                  types={fileTypes}
+                />
+              </div>
+              <div className="button1">
+                <button
+                  onClick={() => {
+                    Tesseract.recognize(file, "eng", {
+                      logger: (m) => console.log(m),
+                    }).then(({ data: { text } }) => {
+                      setInput(text);
+                      console.log(text);
+                    });
+                  }}
+                >
+                  Convert
+                </button>
+              </div>
+            </div>
+            <div className="footer2">
+              <input
+                className="composebar"
+                type="text"
+                autoFocus
+                placeholder="Ask Anythings!"
+                value={input}
+                onKeyUp={onKeyUp}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <div className="send-button" onClick={onSubmits}>
+                <img src={send} alt="send" />
+              </div>
+            </div>
+          </div>
+        </footer>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from "Express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+// create server
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "This is open ai server running on.",
+  });
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "",
+      temperature: 0,
+      max_tokens: 1256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    console.log("Passed", req.body.input);
+    res.status(200).send({
+      message: "Success",
+      bot: response.data.choices[0].text,
+    });
+  } catch (err) {
+    console.log("Failed", req.body.input);
+    console.log(err);
+    res.status(500).send({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
